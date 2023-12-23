ed902f87ca15fc9d5e6de98d18372602f1755c33
madad <alexander.laputko@ya.ru>
2023-12-23 15:21:17 +0300

    Added scrolling toolbar.

---
+++ app/src/main/java/hfa/madad/bitsandpizzas/OrderFragment.kt
@@ -0,0 +1,22 @@
+package hfa.madad.bitsandpizzas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
+
+class OrderFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_order, container, false)
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        // Inflate the layout for this fragment
+        return view
+    }
+}
