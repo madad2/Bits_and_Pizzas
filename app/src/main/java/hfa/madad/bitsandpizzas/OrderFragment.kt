f0426acc66530435a51272caa13b8560be42e9eb
madad <alexander.laputko@ya.ru>
2024-01-10 19:30:36 +0300

    Add Toast message, when you didn't choose an order.

--- app/src/main/java/hfa/madad/bitsandpizzas/OrderFragment.kt
+++ app/src/main/java/hfa/madad/bitsandpizzas/OrderFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class OrderFragment : Fragment() {
     override fun onCreateView(
@@ -16,6 +19,20 @@ class OrderFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_order, container, false)
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+
+        //Слушаем кнопку FAB
+        fab.setOnClickListener {
+            val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
+            val pizzaType = pizzaGroup.checkedRadioButtonId
+            if (pizzaType == -1) {
+                //Пицца не выбрана, вывести сообщение
+                val text = "You need to choose a pizza type"
+                Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
+            } else {
+                //Вывести заказ в Snackbar сообщении
+            }
+        }
         // Inflate the layout for this fragment
         return view
     }
